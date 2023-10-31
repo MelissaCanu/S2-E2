062fe29e7453df19087d92016acaac52ccb0368f
Melissa Canu <melissacanu89@gmail.com>
2023-10-31 16:24:01 +0100

    ex. 10 finished

--- D2.js
+++ D2.js
@@ -145,7 +145,9 @@ if (pariOdispari % 2 == 0) {
 /* ESERCIZIO 10
   Modifica la logica del seguente algoritmo in modo che mostri in console il messaggio corretto in ogni circostanza.*/
 
-let val = 10;
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+let val = 8;
 
 if (val < 5) {
   console.log("Meno di 5");
@@ -157,19 +159,20 @@ if (val >= 10) {
   console.log("Uguale a 10 o maggiore");
 }
 
-/* SCRIVI QUI LA TUA RISPOSTA */
-
 /* ESERCIZIO 11
   Fornito il seguente oggetto, scrivi del codice per aggiungere una proprietà "city", il cui valore sarà "Toronto".
 */
 
-// const me = {
-//   name: "John",
-//   lastName: "Doe",
-//   skills: ["javascript", "html", "css"],
-// };
+const me = {
+  name: "John",
+  lastName: "Doe",
+  skills: ["javascript", "html", "css"],
+};
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+me.city = "Toronto";
+
+console.log(me);
 
 /* ESERCIZIO 12
   Lavorando sempre sull'oggetto precedentemente fornito, scrivi del codice per rimuovere la proprietà "lastName".
@@ -177,11 +180,18 @@ if (val >= 10) {
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+delete me.lastName;
+
+console.log(me);
+
 /* ESERCIZIO 13
   Lavorando sempre sull'oggetto precedentemente fornito, scrivi del codice per rimuovere l'ultimo elemento della proprietà "skills".
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+
+me.skills.pop();
+console.log(me);
 
 /* ESERCIZIO 14
   Scrivi del codice per creare un array inizialmente vuoto. Riempilo successivamente con i numeri da 1 a 10.
@@ -189,8 +199,15 @@ if (val >= 10) {
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+let myArray = [];
+myArray.push(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+console.log(myArray);
+
 /* ESERCIZIO 15
   Scrivi del codice per sostituire l'ultimo elemento dell'array, ovvero il valore 10, con il valore 100.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+
+myArray.splice(9, 1, 100);
+console.log(myArray);
